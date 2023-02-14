4767ddbf5c55f1646b16cfd37ca254c948565a31
n3ur0mancer <46748400+n3ur0mancer@users.noreply.github.com>
2023-02-14 18:07:36 +0100

    Reformatted code

--- forstler_iot-code/forstler_iot-receiver/forstler_iot-receiver.ino
+++ forstler_iot-code/forstler_iot-receiver/forstler_iot-receiver.ino
@@ -1,59 +1,57 @@
 #include "LoRaWan_APP.h"
 #include "Arduino.h"
-#include <Wire.h>               
+#include <Wire.h>
 #include "HT_SSD1306Wire.h"
 
-SSD1306Wire  esp_display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLED); // addr , freq , i2c group , resolution , rst
+SSD1306Wire esp_display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLED); // addr , freq , i2c group , resolution , rst
 
-
-#define RF_FREQUENCY                                868000000 // Hz
-#define TX_OUTPUT_POWER                             14        // dBm
-#define LORA_BANDWIDTH                              0         // [0: 125 kHz, 1: 250 kHz, 2: 500 kHz, 3: Reserved]
-#define LORA_SPREADING_FACTOR                       7         // [SF7..SF12]
-#define LORA_CODINGRATE                             1         // [1: 4/5, 2: 4/6,   3: 4/7,  4: 4/8]
-#define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
-#define LORA_SYMBOL_TIMEOUT                         0         // Symbols
-#define LORA_FIX_LENGTH_PAYLOAD_ON                  false
-#define LORA_IQ_INVERSION_ON                        false
-#define RX_TIMEOUT_VALUE                            1000
-#define BUFFER_SIZE                                 30 // Define the payload size here
+#define RF_FREQUENCY 868000000  // Hz
+#define TX_OUTPUT_POWER 14      // dBm
+#define LORA_BANDWIDTH 0        // [0: 125 kHz, 1: 250 kHz, 2: 500 kHz, 3: Reserved]
+#define LORA_SPREADING_FACTOR 7 // [SF7..SF12]
+#define LORA_CODINGRATE 1       // [1: 4/5, 2: 4/6,   3: 4/7,  4: 4/8]
+#define LORA_PREAMBLE_LENGTH 8  // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT 0   // Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON false
+#define LORA_IQ_INVERSION_ON false
+#define RX_TIMEOUT_VALUE 1000
+#define BUFFER_SIZE 30 // Define the payload size here
 
 char txpacket[BUFFER_SIZE];
 char rxpacket[BUFFER_SIZE];
 
 static RadioEvents_t RadioEvents;
 int16_t txNumber;
-int16_t rssi,rxSize;
+int16_t rssi, rxSize;
 bool lora_idle = true;
 
-void setup() {
-    Serial.begin(115200);
-    Mcu.begin();
-    
-    delay(100);
-    esp_display.init();
-    esp_display.setTextAlignment(TEXT_ALIGN_LEFT);
-    esp_display.setFont(ArialMT_Plain_10);
+void setup()
+{
+  Serial.begin(115200);
+  Mcu.begin();
 
-    txNumber=0;
-    rssi=0;
-  
-    RadioEvents.RxDone = OnRxDone;
-    Radio.Init( &RadioEvents );
-    Radio.SetChannel( RF_FREQUENCY );
-    Radio.SetRxConfig( MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR, LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH, LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON,0, true, 0, 0, LORA_IQ_INVERSION_ON, true );
+  delay(100);
+  esp_display.init();
+  esp_display.setTextAlignment(TEXT_ALIGN_LEFT);
+  esp_display.setFont(ArialMT_Plain_10);
+
+  txNumber = 0;
+  rssi = 0;
+
+  RadioEvents.RxDone = OnRxDone;
+  Radio.Init(&RadioEvents);
+  Radio.SetChannel(RF_FREQUENCY);
+  Radio.SetRxConfig(MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR, LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH, LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON, 0, true, 0, 0, LORA_IQ_INVERSION_ON, true);
 }
-
-
 
 void loop()
 {
-  Radio.IrqProcess( );
+  Radio.IrqProcess();
 
   esp_display.clear();
   esp_display.setFont(ArialMT_Plain_24);
 
-  if(lora_idle)
+  if (lora_idle)
   {
     lora_idle = false;
     Serial.println("RX mode on...");
@@ -68,18 +66,15 @@ void loop()
   delay(2500);
 }
 
-
-
-void OnRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr )
+void OnRxDone(uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr)
 {
-    rssi=rssi;
-    rxSize=size;
+  rssi = rssi;
+  rxSize = size;
 
-    memcpy(rxpacket, payload, size );
-    rxpacket[size]='\0';
-    Radio.Sleep( );
-    Serial.printf("\r\nReceived packet: \"%s\" with rssi %d , length %d\r\n",rxpacket,rssi,rxSize);
-		
-    lora_idle = true;
+  memcpy(rxpacket, payload, size);
+  rxpacket[size] = '\0';
+  Radio.Sleep();
+  Serial.printf("\r\nReceived packet: \"%s\" with rssi %d , length %d\r\n", rxpacket, rssi, rxSize);
+
+  lora_idle = true;
 }
-
--- forstler_iot-code/forstler_iot-transmitter/forstler_iot-transmitter.ino
+++ forstler_iot-code/forstler_iot-transmitter/forstler_iot-transmitter.ino
@@ -7,22 +7,22 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-#define RF_FREQUENCY                                868000000 // Hz
-#define TX_OUTPUT_POWER                             10        // dBm
-#define LORA_BANDWIDTH                              0         // [0: 125 kHz, 1: 250 kHz, 2: 500 kHz, 3: Reserved]
-#define LORA_SPREADING_FACTOR                       7         // [SF7..SF12]
-#define LORA_CODINGRATE                             1         // [1: 4/5, 2: 4/6, 3: 4/7, 4: 4/8]
-#define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
-#define LORA_SYMBOL_TIMEOUT                         0         // Symbols
-#define LORA_FIX_LENGTH_PAYLOAD_ON                  false
-#define LORA_IQ_INVERSION_ON                        false
-#define RX_TIMEOUT_VALUE                            1000
-#define BUFFER_SIZE                                 30 // Define the payload size here
+#define RF_FREQUENCY 868000000  // Hz
+#define TX_OUTPUT_POWER 10      // dBm
+#define LORA_BANDWIDTH 0        // [0: 125 kHz, 1: 250 kHz, 2: 500 kHz, 3: Reserved]
+#define LORA_SPREADING_FACTOR 7 // [SF7..SF12]
+#define LORA_CODINGRATE 1       // [1: 4/5, 2: 4/6, 3: 4/7, 4: 4/8]
+#define LORA_PREAMBLE_LENGTH 8  // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT 0   // Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON false
+#define LORA_IQ_INVERSION_ON false
+#define RX_TIMEOUT_VALUE 1000
+#define BUFFER_SIZE 30 // Define the payload size here
 
 #define SDA_PIN 19
 #define SCL_PIN 20
 
-//BMP280 bmp280;
+// BMP280 bmp280;
 BH1750 lightMeter;
 DHT dht(33, DHT22);
 OneWire oneWire(21);
@@ -34,10 +34,11 @@ double txNumber;
 bool lora_idle = true;
 
 static RadioEvents_t RadioEvents;
-void OnTxDone( void );
-void OnTxTimeout( void );
+void OnTxDone(void);
+void OnTxTimeout(void);
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   Mcu.begin();
 
@@ -47,7 +48,7 @@ void setup() {
   // BMP280
   // bmp280.begin();
 
-   // DallasTemperature
+  // DallasTemperature
   dallas.begin();
 
   // BH1750
@@ -57,51 +58,54 @@ void setup() {
   dht.begin();
   delay(50);
 
-
   txNumber = 0;
 
   RadioEvents.TxDone = OnTxDone;
   RadioEvents.TxTimeout = OnTxTimeout;
 
-  Radio.Init( &RadioEvents );
-  Radio.SetChannel( RF_FREQUENCY );
-  Radio.SetTxConfig( MODEM_LORA, TX_OUTPUT_POWER, 0, LORA_BANDWIDTH,
-                     LORA_SPREADING_FACTOR, LORA_CODINGRATE,
-                     LORA_PREAMBLE_LENGTH, LORA_FIX_LENGTH_PAYLOAD_ON,
-                     true, 0, 0, LORA_IQ_INVERSION_ON, 3000 );
+  Radio.Init(&RadioEvents);
+  Radio.SetChannel(RF_FREQUENCY);
+  Radio.SetTxConfig(MODEM_LORA, TX_OUTPUT_POWER, 0, LORA_BANDWIDTH,
+                    LORA_SPREADING_FACTOR, LORA_CODINGRATE,
+                    LORA_PREAMBLE_LENGTH, LORA_FIX_LENGTH_PAYLOAD_ON,
+                    true, 0, 0, LORA_IQ_INVERSION_ON, 3000);
 
   Serial.println("All sensors and LoRa are ready");
 }
 
-void loop() {
-  if (lora_idle == true) {
+void loop()
+{
+  if (lora_idle == true)
+  {
     delay(10000);
 
-// float temperature = bmp280.getTemperature();
-// float pressure = bmp280.getPressure();
-dallas.requestTemperatures();
-float dallasTemp = dallas.getTempCByIndex(0);
-float dhtTemp = dht.readTemperature();
-float humidity = dht.readHumidity();
-float light = lightMeter.readLightLevel();
+    // float temperature = bmp280.getTemperature();
+    // float pressure = bmp280.getPressure();
+    dallas.requestTemperatures();
+    float dallasTemp = dallas.getTempCByIndex(0);
+    float dhtTemp = dht.readTemperature();
+    float humidity = dht.readHumidity();
+    float light = lightMeter.readLightLevel();
 
-sprintf(txpacket, "DHTTemperature: %0.2f, Humidity: %0.2f, LUX: %0.2f, DallasTemperature: %0.2f", dhtTemp, humidity, light, dallasTemp, txNumber); //start a package
+    sprintf(txpacket, "DHTTemperature: %0.2f, Humidity: %0.2f, LUX: %0.2f, DallasTemperature: %0.2f", dhtTemp, humidity, light, dallasTemp, txNumber); // start a package
 
-Serial.printf("\r\nSending packet... Length: %d\r\n", txpacket, strlen(txpacket));
+    Serial.printf("\r\nSending packet... Length: %d\r\n", txpacket, strlen(txpacket));
 
-Radio.Send((uint8_t *) txpacket, strlen(txpacket)); //send the package out
-lora_idle = false;
-}
-Radio.IrqProcess();
-}
-
-void OnTxDone( void ) {
-Serial.println("TX Done");
-lora_idle = true;
+    Radio.Send((uint8_t *)txpacket, strlen(txpacket)); // send the package out
+    lora_idle = false;
+  }
+  Radio.IrqProcess();
 }
 
-void OnTxTimeout( void ) {
-Radio.Sleep();
-Serial.println("TX Timeout");
-lora_idle = true;
+void OnTxDone(void)
+{
+  Serial.println("TX Done");
+  lora_idle = true;
+}
+
+void OnTxTimeout(void)
+{
+  Radio.Sleep();
+  Serial.println("TX Timeout");
+  lora_idle = true;
 }
